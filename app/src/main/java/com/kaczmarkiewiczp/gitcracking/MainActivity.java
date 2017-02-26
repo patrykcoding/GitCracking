bbb9c5e3289425d2f8a56513fdcf1cb17f7bacdc
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-02-26 13:40:19 -0700

    Add animation

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/MainActivity.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/MainActivity.java
@@ -1,14 +1,24 @@
 package com.kaczmarkiewiczp.gitcracking;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.Toast;
 import org.eclipse.egit.github.core.Authorization;
+import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.OAuthService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import java.io.IOException;
+
+import xdroid.toaster.Toaster;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,21 +38,22 @@ public class MainActivity extends AppCompatActivity {
         String username = usernameView.getText().toString().trim();
         String password = passwordView.getText().toString();
 
+        Context context = getApplicationContext();
         if (username.length() == 0) {
-            Context context = getApplicationContext();
+            shake(usernameView);
             Toast toast = Toast.makeText(context, "Please provide your username", Toast.LENGTH_SHORT);
             toast.show();
             return;
         } else if (password.length() == 0) {
-            Context context = getApplicationContext();
+            shake(passwordView);
             Toast toast = Toast.makeText(context, "Please provide your password", Toast.LENGTH_SHORT);
             toast.show();
             return;
         }
-
     }
 
-    private void doLogin() {
-
+    private void shake(EditText element) {
+        Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+        element.startAnimation(shake);
     }
 }
