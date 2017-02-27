f4d2506c0be4fcf55b3f6c09f2c679a377ae838d
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-02-26 17:33:05 -0700

    Basic user authentication

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/MainActivity.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kaczmarkiewiczp.gitcracking;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +21,8 @@ import org.eclipse.egit.github.core.service.OAuthService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.UUID;
 
 import xdroid.toaster.Toaster;
 
@@ -50,10 +56,75 @@ public class MainActivity extends AppCompatActivity {
             toast.show();
             return;
         }
+        new LoginTask().execute(username, password);
     }
 
+    /*
+     * Animate an element
+     */
     private void shake(EditText element) {
         Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
         element.startAnimation(shake);
+    }
+
+    public class LoginTask extends AsyncTask<String, Void, Authorization> {
+
+        private String username;
+        private String password;
+
+        @Override
+        protected Authorization doInBackground(String... credentials) {
+            this.username = credentials[0];
+            this.password = credentials[1];
+            GitHubClient client = new GitHubClient();
+
+            String appName = getResources().getString(R.string.app_name);
+            client.setCredentials(username, password);
+            client.setUserAgent(appName);
+            String description = appName + " - " + Build.MANUFACTURER + " " + Build.MODEL;
+
+            OAuthService authService = new OAuthService(client);
+
+            try {
+                for (Authorization authorization : authService.getAuthorizations()) {
+                    String note = authorization.getNote();
+
+                    if (note != null && note.startsWith(appName)) {
+                        authService.deleteAuthorization(authorization.getId());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Authorization auth = new Authorization();
+            auth.setNote(description);
+            auth.setUrl("https://github.com/kaczmarkiewiczp/GitCracking"); // TODO strings.xml
+            auth.setScopes(Arrays.asList("user", "repo", "gist"));
+
+            try {
+                return authService.createAuthorization(auth);
+            } catch (IOException e) {
+                e.printStackTrace();
+                // TODO check for two factor authentication
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Authorization authorization) {
+            if (authorization == null) {
+                // TODO
+                return;
+            }
+
+            // TODO pref name should be a public constant in pref class
+            SharedPreferences sharedPreferences = getSharedPreferences("GitCrackingPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            // TODO create interface constants for 'token' and 'login'
+            editor.putString("token", authorization.getToken());
+            editor.putString("login", username);
+            editor.apply();
+        }
     }
 }
