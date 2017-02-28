181f0a48c3e1094788308d8da0613d9b80f2570d
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-02-28 13:37:19 -0700

    Add loading dialog when signing in

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/MainActivity.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
