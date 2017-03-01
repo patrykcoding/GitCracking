26f25efadab688af49a13eb234251a507f7d7bed
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-01 12:38:08 -0700

    Add toolbar and change default theme for the app

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/Dashboard.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/Dashboard.java
@@ -1,7 +1,9 @@
 package com.kaczmarkiewiczp.gitcracking;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_dashboard_toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("Dashboard");
+        setSupportActionBar(toolbar);
     }
 }
