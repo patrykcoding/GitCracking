b33978474c8adc1e744664500ae605283052e133
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-01 15:21:22 -0700

    Add navigation drawer in the dashboard

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/Dashboard.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/Dashboard.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+
 public class Dashboard extends AppCompatActivity {
 
     @Override
@@ -15,5 +20,23 @@ public class Dashboard extends AppCompatActivity {
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setTitle("Dashboard");
         setSupportActionBar(toolbar);
+        setupNavigationDrawer(toolbar);
+    }
+
+    private void setupNavigationDrawer(Toolbar toolbar) {
+        new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Dashboard"),
+                        new PrimaryDrawerItem().withName("Repositories"),
+                        new PrimaryDrawerItem().withName("Issues"),
+                        new PrimaryDrawerItem().withName("Pull Requests"),
+                        new PrimaryDrawerItem().withName("People"),
+                        new PrimaryDrawerItem().withName("Bookmarks"),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName("Settings")
+                )
+                .build();
     }
 }
