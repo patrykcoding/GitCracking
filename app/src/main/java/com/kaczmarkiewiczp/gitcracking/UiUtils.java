4f644b0ffc402b1683ad7e8aa2b58a1886d556d3
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-02 12:58:31 -0700

    Update nav drawer icons and add header image

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/UiUtils.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/UiUtils.java
@@ -21,14 +21,14 @@ class UiUtils {
                 .withToolbar(toolbar)
                 .withAccountHeader(createAccountHeader(activity))
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName("Dashboard").withIcon(activity.getDrawable(R.drawable.ic_home_black_24dp)),
-                        new PrimaryDrawerItem().withName("Repositories").withIcon(activity.getDrawable(R.drawable.ic_octicon_repo)),
-                        new PrimaryDrawerItem().withName("Issues").withIcon(activity.getDrawable(R.drawable.ic_octicon_issue_24dp)),
-                        new PrimaryDrawerItem().withName("Pull Requests").withIcon(activity.getDrawable(R.drawable.ic_octicon_git_pull_request)),
-                        new PrimaryDrawerItem().withName("People").withIcon(activity.getDrawable(R.drawable.ic_people_black_24dp)),
-                        new PrimaryDrawerItem().withName("Bookmarks").withIcon(activity.getDrawable(R.drawable.ic_star_black_24dp)),
+                        new PrimaryDrawerItem().withName("Dashboard").withIcon(activity.getDrawable(R.drawable.ic_home)),
+                        new PrimaryDrawerItem().withName("Repositories").withIcon(activity.getDrawable(R.drawable.ic_git)),
+                        new PrimaryDrawerItem().withName("Issues").withIcon(activity.getDrawable(R.drawable.ic_alert_circle_outline)),
+                        new PrimaryDrawerItem().withName("Pull Requests").withIcon(activity.getDrawable(R.drawable.ic_source_pull)),
+                        new PrimaryDrawerItem().withName("People").withIcon(activity.getDrawable(R.drawable.ic_account_multiple)),
+                        new PrimaryDrawerItem().withName("Bookmarks").withIcon(activity.getDrawable(R.drawable.ic_star)),
                         new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName("Settings").withIcon(activity.getDrawable(R.drawable.ic_settings_black_24dp))
+                        new SecondaryDrawerItem().withName("Settings").withIcon(activity.getDrawable(R.drawable.ic_settings))
                 );
         return drawerBuilder.build();
     }
@@ -36,6 +36,7 @@ class UiUtils {
     static private AccountHeader createAccountHeader(Activity activity) {
         AccountHeader accountHeader = new AccountHeaderBuilder()
                 .withActivity(activity)
+                .withHeaderBackground(R.drawable.header2)
                 .addProfiles(
                         new ProfileDrawerItem().withName("Patryk").withEmail("kaczmarkiewiczp@mymacewan.ca"),
                         new ProfileSettingDrawerItem().withName("Manage Accounts")
