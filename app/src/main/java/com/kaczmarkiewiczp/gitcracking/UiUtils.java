bd5e05b3c3ffc94757cf1830ff898de32c0dfb0e
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-02 12:21:09 -0700

    Add account header in navigation drawer

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/UiUtils.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/UiUtils.java
@@ -1,12 +1,17 @@
 package com.kaczmarkiewiczp.gitcracking;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 class UiUtils {
@@ -14,6 +19,7 @@ class UiUtils {
         DrawerBuilder drawerBuilder = new DrawerBuilder();
         drawerBuilder.withActivity(activity)
                 .withToolbar(toolbar)
+                .withAccountHeader(createAccountHeader(activity))
                 .addDrawerItems(
                         new PrimaryDrawerItem().withName("Dashboard").withIcon(activity.getDrawable(R.drawable.ic_home_black_24dp)),
                         new PrimaryDrawerItem().withName("Repositories").withIcon(activity.getDrawable(R.drawable.ic_octicon_repo)),
@@ -25,5 +31,17 @@ class UiUtils {
                         new SecondaryDrawerItem().withName("Settings").withIcon(activity.getDrawable(R.drawable.ic_settings_black_24dp))
                 );
         return drawerBuilder.build();
+    }
+
+    static private AccountHeader createAccountHeader(Activity activity) {
+        AccountHeader accountHeader = new AccountHeaderBuilder()
+                .withActivity(activity)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Patryk").withEmail("kaczmarkiewiczp@mymacewan.ca"),
+                        new ProfileSettingDrawerItem().withName("Manage Accounts")
+                )
+                .withTextColor(Color.BLACK)
+                .build();
+        return accountHeader;
     }
 }
