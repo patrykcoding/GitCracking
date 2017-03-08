e4146561c993cfb9d55e2c89d26f81d880a24dfe
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-07 17:34:35 -0700

    Add on click listeners for navigation drawer items

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/NavBarUtils.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/NavBarUtils.java
@@ -1,8 +1,10 @@
 package com.kaczmarkiewiczp.gitcracking;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -13,40 +15,92 @@ import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 class NavBarUtils {
 
     public PrimaryDrawerItem dashboard = new PrimaryDrawerItem()
             .withName("Dashboard")
             .withIcon(R.drawable.ic_home)
-            .withIdentifier(1);
+            .withIdentifier(1)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
     public PrimaryDrawerItem repositories = new PrimaryDrawerItem().withName("Repositories")
             .withIcon(R.drawable.ic_git)
-            .withIdentifier(2);
+            .withIdentifier(2)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
     public PrimaryDrawerItem issues = new PrimaryDrawerItem()
             .withName("Issues")
             .withIcon(R.drawable.ic_alert_circle_outline)
-            .withIdentifier(3);
+            .withIdentifier(3)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
     public PrimaryDrawerItem pullRequests = new PrimaryDrawerItem()
             .withName("Pull Requests")
             .withIcon(R.drawable.ic_source_pull)
-            .withIdentifier(4);
+            .withIdentifier(4)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
     public PrimaryDrawerItem people = new PrimaryDrawerItem()
             .withName("People")
             .withIcon(R.drawable.ic_account_multiple)
-            .withIdentifier(5);
+            .withIdentifier(5)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
     public PrimaryDrawerItem bookmarks = new PrimaryDrawerItem()
             .withName("Bookmarks")
             .withIcon(R.drawable.ic_star)
-            .withIdentifier(6);
+            .withIdentifier(6)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
     public SecondaryDrawerItem settings = new SecondaryDrawerItem()
             .withName("Settings")
             .withIcon(R.drawable.ic_settings)
-            .withIdentifier(7);
+            .withIdentifier(7)
+            .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                @Override
+                public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                    drawerItemClicked(view, position, drawerItem);
+                    return false;
+                }
+            });
 
     private Drawer drawer;
+    private Activity activity;
 
     public NavBarUtils(Activity activity, Toolbar toolbar) {
+        this.activity = activity;
         DrawerBuilder drawerBuilder = new DrawerBuilder();
         drawerBuilder.withActivity(activity)
                 .withToolbar(toolbar)
@@ -67,8 +121,19 @@ class NavBarUtils {
     public Drawer getDrawer() {
         return this.drawer;
     }
+
+    private void drawerItemClicked(View view, int position, IDrawerItem drawerItem) {
+        int drawerIdentifier = (int) drawerItem.getIdentifier();
+
+        switch (drawerIdentifier) {
+            case 1:
+                activity.startActivity(new Intent(activity, Dashboard.class));
+                break;
+        }
+    }
+
     private AccountHeader createAccountHeader(Activity activity) {
-        AccountHeader accountHeader = new AccountHeaderBuilder()
+        return new AccountHeaderBuilder()
                 .withActivity(activity)
                 .withHeaderBackground(R.drawable.header2)
                 .addProfiles(
@@ -77,6 +142,5 @@ class NavBarUtils {
                 )
                 .withTextColor(Color.BLACK)
                 .build();
-        return accountHeader;
     }
 }
