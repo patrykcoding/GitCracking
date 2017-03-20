af6ac3dd5f53b8e460fbd2c6cabb355b4def3af8
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-20 11:50:55 -0600

    Send in Repository object to adapter
    Closes #50
    Closes #64

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/adapter/RepositoriesAdapter.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/adapter/RepositoriesAdapter.java
@@ -3,6 +3,8 @@ package com.kaczmarkiewiczp.gitcracking.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import com.kaczmarkiewiczp.gitcracking.R;
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 
+import org.eclipse.egit.github.core.Repository;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -18,24 +21,13 @@ import java.util.ArrayList;
 
 public class RepositoriesAdapter extends RecyclerView.Adapter<RepositoriesAdapter.ViewHolder> implements FastScrollRecyclerView.SectionedAdapter  {
 
-    private ArrayList<String> repositoriesName;
-    private ArrayList<String> repositoriesOwner;
-    private ArrayList<String> repositoriesDescription;
-    private ArrayList<Boolean> repositoriesPrivate;
-    private ArrayList<String> repositoriesLanguage;
-    private ArrayList<String> repositoriesForks;
-    private ArrayList<String> repositoriesWatchers;
-    private ArrayList<String> repositoriesSize;
+    private ArrayList<Repository> repositories;
+    private Context context;
 
-    public RepositoriesAdapter() {
-        repositoriesName = new ArrayList<>();
-        repositoriesOwner = new ArrayList<>();
-        repositoriesDescription = new ArrayList<>();
-        repositoriesPrivate = new ArrayList<>();
-        repositoriesLanguage = new ArrayList<>();
-        repositoriesForks = new ArrayList<>();
-        repositoriesWatchers = new ArrayList<>();
-        repositoriesSize = new ArrayList<>();
+    public RepositoriesAdapter(Context context) {
+        repositories = new ArrayList<>();
+        this.context = context;
+
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -75,15 +67,17 @@ public class RepositoriesAdapter extends RecyclerView.Adapter<RepositoriesAdapte
 
     @Override
     public void onBindViewHolder(RepositoriesAdapter.ViewHolder holder, int position) {
-        String name = repositoriesName.get(position);
-        String owner = repositoriesOwner.get(position);
-        String description = repositoriesDescription.get(position);
-        Boolean isPrivate = repositoriesPrivate.get(position);
-        String language = repositoriesLanguage.get(position);
-        String forks = repositoriesForks.get(position);
-        String watchers = repositoriesWatchers.get(position);
-        String size = repositoriesSize.get(position);
+        /* retrieve required data from the Repository structure */
+        String name = repositories.get(position).getName();
+        String owner = repositories.get(position).getOwner().getLogin();
+        String description = repositories.get(position).getDescription();
+        Boolean isPrivate = repositories.get(position).isPrivate();
+        String language = repositories.get(position).getLanguage();
+        Integer forks = repositories.get(position).getForks();
+        Integer watchers = repositories.get(position).getWatchers();
+        Integer size = repositories.get(position).getSize();
 
+        /* Set appropriate elements in out view */
         holder.textViewName.setText(owner + "/" + name);
         if (description == null || description.isEmpty()) {
             holder.textViewDescription.setVisibility(View.GONE);
@@ -95,9 +89,10 @@ public class RepositoriesAdapter extends RecyclerView.Adapter<RepositoriesAdapte
         } else {
             holder.textViewLanguage.setText(language);
         }
-        holder.textViewForks.setText(forks);
-        holder.textViewWatchers.setText(watchers);
-        holder.textViewSize.setText(size);
+        holder.textViewForks.setText(forks.toString());
+        holder.textViewWatchers.setText(watchers.toString());
+        String s = Formatter.formatShortFileSize(context, size.longValue() * 1024);
+        holder.textViewSize.setText(s);
         if (isPrivate)
             holder.linearLayoutPrivate.setVisibility(View.VISIBLE);
         else
@@ -106,53 +101,18 @@ public class RepositoriesAdapter extends RecyclerView.Adapter<RepositoriesAdapte
 
     @Override
     public int getItemCount() {
-        if (repositoriesName == null) {
+        if (repositories == null) {
             return 0;
         }
-        return repositoriesName.size();
+        return repositories.size();
     }
 
-    public void addRepositoriesName(ArrayList<String> names) {
-        repositoriesName.addAll(names);
-    }
-
-    public void addRepositoriesOwner(ArrayList<String> owners) {
-        repositoriesOwner.addAll(owners);
-    }
-
-    public void addDescriptions(ArrayList<String> descritpions) {
-        repositoriesDescription.addAll(descritpions);
-    }
-
-    public void addLanguages(ArrayList<String> languages) {
-        repositoriesLanguage.addAll(languages);
-    }
-
-    public void addForks(ArrayList<String> forks) {
-        repositoriesForks.addAll(forks);
-    }
-
-    public void addWatchers(ArrayList<String> watchers) {
-        repositoriesWatchers.addAll(watchers);
-    }
-
-    public void addSizes(ArrayList<String> sizes) {
-        repositoriesSize.addAll(sizes);
-    }
-
-    public void addPrivates(ArrayList<Boolean> isPrivate) {
-        repositoriesPrivate.addAll(isPrivate);
+    public void setRepositories(ArrayList<Repository> repositories) {
+        this.repositories.addAll(repositories);
     }
 
     public void clearView() {
-        repositoriesName.clear();
-        repositoriesOwner.clear();
-        repositoriesDescription.clear();
-        repositoriesPrivate.clear();
-        repositoriesLanguage.clear();
-        repositoriesForks.clear();
-        repositoriesWatchers.clear();
-        repositoriesSize.clear();
+        repositories.clear();
     }
 
     public void updateView() {
@@ -162,6 +122,8 @@ public class RepositoriesAdapter extends RecyclerView.Adapter<RepositoriesAdapte
     @NonNull
     @Override
     public String getSectionName(int position) {
-        return repositoriesName.get(position).substring(0,1).toUpperCase();
+        String repositoryName = repositories.get(position).getName();
+        String firstLetter = repositoryName.substring(0, 1).toUpperCase();
+        return firstLetter;
     }
 }
