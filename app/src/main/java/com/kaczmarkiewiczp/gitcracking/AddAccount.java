3803246168c28f928ac70aa69b1f23588105e10a
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-03-28 16:31:31 -0600

    Fix back button not working
    Closes #121

--- app/src/main/java/com/kaczmarkiewiczp/gitcracking/AddAccount.java
+++ app/src/main/java/com/kaczmarkiewiczp/gitcracking/AddAccount.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -73,6 +74,17 @@ public class AddAccount extends AppCompatActivity {
                 loginButtonClicked(v);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void loginButtonClicked(View view) {
